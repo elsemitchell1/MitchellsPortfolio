019fa4d489fe080122d8a4d4eabcef0a1df7e494
Mitchell Else <elsemitchell1@gmail.com>
2024-05-30 21:07:31 -0600

    text colour

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -7,10 +7,10 @@ const Skills = (props) => {
             <h1 className='text-4xl font-bold underline'>{props.heading}</h1>
             <div className='flex flex-wrap justify-center mt-12 mb-6 gap-y-16 gap-x-10'>
                 {props.skills.map((skill, index) => (
-                    <div className='flex items-center space-x-2'>
-                        <h4 className='text-4xl'>{skill.icon}</h4>
-                        <h4 className='text-lg font-bold'>{skill.skill}</h4>
-                        </div>
+                  <div className='flex items-center space-x-2'>
+                    <h4 className='text-4xl'>{skill.icon}</h4>
+                    <h4 className='text-lg font-bold'>{skill.skill}</h4>
+                  </div>
                 ))}
             </div>
         </div>
--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -3,15 +3,17 @@ import React from 'react';
 const Timeline = (props) => {
     
   return (
-    <div className="relative flex flex-col items-center w-full max-w-5xl m-10 mx-auto">
+    <div className="relative flex flex-col items-center w-full max-w-5xl m-10 mx-auto text-white">
       <div className="absolute h-full transform -translate-x-1/2 border-l-2 border-gray-300 z-5 left-1/2"></div>
       {props.timelineData.map((event, index) => (
         <div key={event.id} className={`z-10 flex-col-reverse flex w-full lg:mb-8 ${index % 2 ? 'lg:flex-row-reverse' : 'lg:flex-row'}`}>
           <div className={`flex justify-center w-full lg:w-1/2 ${index % 2 ? 'lg:justify-start lg:pl-8' : 'lg:pr-8 lg:justify-end'}`}>
-            <div className="w-full max-w-sm p-4 bg-gradient-to-r from-[#FFA500] to-[#FFFF00] rounded-md shadow-lg">
-              <h3 className="font-semibold text-md lg:text-lg">{event.title}</h3>
-              <time className="block text-xs text-gray-500 lg:text-sm">{event.date}</time>
-              <p className="mt-2 text-xs text-gray-700 lg:text-sm">{event.description}</p>
+            <div className="w-full max-w-sm p-px bg-gradient-to-r from-[#FFA500] to-[#FFFF00] rounded-md shadow-lg">
+              <div className='w-full h-full p-4 bg-black rounded-md'>
+                <h3 className="font-semibold text-md lg:text-lg">{event.title}</h3>
+                <time className="block text-xs text-gradient lg:text-sm">{event.date}</time>
+                <p className="mt-2 text-xs lg:text-sm">{event.description}</p>
+              </div>
             </div>
           </div>
           <div className="relative flex items-center justify-center w-12 mx-auto my-8 lg:my-0">
