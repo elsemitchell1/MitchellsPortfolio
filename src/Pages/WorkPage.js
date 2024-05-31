8cd2cb861d0dda0a24f2d61131b47cc01f990146
Mitchell Else <elsemitchell1@gmail.com>
2024-05-30 19:30:29 -0600

    css

--- src/Pages/WorkPage.js
+++ src/Pages/WorkPage.js
@@ -16,7 +16,7 @@ const WorkPage = () => {
   return (
     <div className='m-10'>
         <div className='flex justify-center'>
-          <h1 className='mb-6 text-4xl font-bold text-white underline lg:text-7xl'>Work Experience</h1>
+          <h1 className='mb-6 text-5xl font-bold text-center text-white underline lg:text-7xl'>Work Experience</h1>
         </div>
         <Timeline timelineData={timelineData} />
     </div>
