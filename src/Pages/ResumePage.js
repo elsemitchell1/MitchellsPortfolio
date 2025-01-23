6ba7497c9f4e1def808767e932a2b65528008d0e
Mitchell Else <elsemitchell1@gmail.com>
2025-01-22 21:12:05 -0700

    resume updated

--- src/Pages/ResumePage.js
+++ src/Pages/ResumePage.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Resume from '../Images/Mitchell Else Resume 2024.webp'
+import ResumePage1 from '../Images/Mitchell Else Resume 2025p1.webp';
+import ResumePage2 from '../Images/Mitchell Else Resume 2025p2.webp';
 
 const ResumePage = () => {
   return (
     <div className='flex justify-center w-full p-10'>
-      <div className='p-[3px] md:p-1 bg-gradient-to-r from-[#FFA500] to-[#FFFF00] rounded-xl'>
-        <img alt='Resume' className='w-full max-w-5xl rounded-xl md:rounded-lg' src={Resume} />
+      <div className='flex-col'>
+        <div className='mb-10 p-[3px] md:p-1 bg-gradient-to-r from-[#FFA500] to-[#FFFF00] rounded-xl'>
+          <img alt='Resume' className='w-full max-w-5xl rounded-xl md:rounded-lg' src={ResumePage1} />
+        </div>
+        <div className='p-[3px] md:p-1 bg-gradient-to-r from-[#FFA500] to-[#FFFF00] rounded-xl'>
+          <img alt='Resume' className='w-full max-w-5xl rounded-xl md:rounded-lg' src={ResumePage2} />
+        </div>
       </div>
     </div>
   )
